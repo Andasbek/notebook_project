1d9982a000629aded520d1fd1d1b9a03b6d6dec5
kazybekandas <andasbek.kazybek@gmail.com>
2025-08-05 14:45:42 +0500

    финальная версия проекта

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+CATEGORIES = ["work", "study", "sport", "personal"]
+
 def notes_home(request):
     return render(request, 'notes/home.html')
 
@@ -11,3 +13,6 @@ def category_notes(request, category):
 
 def note_detail(request, note_id):
     return render(request, 'notes/note_detail.html', {'note_id': note_id})
+
+def category_list(request):
+    return render(request, 'notes/category_list.html', {"categories": CATEGORIES})
